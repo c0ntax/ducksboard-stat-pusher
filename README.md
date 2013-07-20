8277430b9e04a102675900db6debfe620fe78f6b
Dan Burzynski <dan@moo.com>
2013-07-20 16:53:42 +0100

    Ability to push ints to Ducksboard

--- README.md
+++ README.md
@@ -2,3 +2,17 @@ ducksboard-stat-pusher
 ======================
 
 A simple script to push arbitrary stats into Ducksboard
+
+Required CPAN Modules
+=====================
+
+Getopt::Long
+File::Touch
+Config::Any
+LWP::UserAgent
+JSON::XS
+
+Usage
+=====
+
+perl ducksboard-stat-pusher.pl -v
--- src/ducksboard-stat-pusher.pl
+++ src/ducksboard-stat-pusher.pl
@@ -13,7 +13,11 @@ use strict;
 use Getopt::Long;
 use OfficeHacksHelpers::Utils;
 use Config::Any;
+use LWP::UserAgent;
+use JSON::XS;
+
 use Data::Dumper;
+
 
 my $verbose = 0;
 my $pushId = undef;
@@ -46,6 +50,18 @@ for (@{Config::Any->load_files({ files => [$configFile], use_ext => 1 })}) {
 
 my $rDatabases = getDataFromConfig('database', $rConfig);
 my $rStats = getDataFromConfig('stat', $rConfig);
+my $apiKey = $rConfig->{ducksboard}->{key};
+
+# Fire up our database connections
+
+while (my ($id, $rDatabase) = each(%$rDatabases)) {
+    if ($rDatabase->{type} eq 'mysql') {
+        my $dsn = 'DBI:mysql:database=' . $rDatabase->{name} . ';host=' . $rDatabase->{host} . ';port=' . $rDatabase->{port};
+        $rDatabase->{dbh} = $util->dbConnect($dsn, $rDatabase->{user}, $rDatabase->{password});
+    } else {
+        die ('Cuurently unsupported database type: [' . $rDatabase->{type} . ']');
+    }
+}
 
 # Now that we have the config, we can start to do the hard work
 
@@ -60,6 +76,7 @@ if (defined($pushId)) {
     $rStatsToProcess = $rStats;
 }
 
+
 	while (my ($key, $rJob) = each(%$rStatsToProcess)) {
 		$util->vPrint('Processing ' . $key . ':' . $rJob->{name} . '...');
 		if (&isTimeForJob($key, $rJob)) {
@@ -73,20 +90,105 @@ if (defined($pushId)) {
 		}
 	}    
 
-
-
-#print Dumper($rConfig);
-#print Dumper($rDatabases);
-#print Dumper($rStats);
-
-
 exit();
 
 sub processJob {
     my ($key, $rJob) = @_;
 
+    my %typeMap = (
+        'counter' => 'intValue',
+        'gauge' => 'percentValue',
+        'graph' => 'timedValue',
+        'bar' => 'intValue',
+        'box' => 'intValue',
+        'pin' => 'intValue',
+        'image' => 'image',
+        'status' => 'status',
+        'text' => 'text',
+        'timeline' => 'timeline',
+        'leaderboard' => 'leaderboard',
+        'funnel' => 'funnel',
+        'completion' => 'completion'
+    );
 
-    print Dumper($rJob);
+    my $jobType = undef;
+    if (defined($typeMap{$rJob->{'slot-kind'}})) {
+        $jobType = $typeMap{$rJob->{'slot-kind'}};
+    } else {
+        die ('Unknown slot kind ' . $rJob->{'slot-kind'});
+    }
+
+    my $data = undef;
+    if ($jobType eq 'intValue') {
+        $data = &processIntValue($key, $rJob);
+    } else {
+        die ('Slot kind of ' . $rJob->{'slot-kind'} . ' unsupported at this time');
+    }
+
+    if ($verbose) {
+        if (!defined($data)) {
+            print '[NULL]...';
+            return 1;
+        } elsif (ref($data) eq 'ARRAY') {
+            my @values = ();
+            foreach my $valueRef (@$data) {
+                push(@values, $valueRef->{value});
+            }
+
+            print '[' . join(', ', @values) . ']...';
+        } elsif (defined($data->{value})) {
+            if (ref($data->{value}) eq '') {
+                print '[' . $data->{value} . ']...';
+            } elsif (ref($data->{value}) eq 'HASH' && $data->{value}->{board}) {
+                my @lines = ();
+                foreach my $stat (@{$data->{value}->{board}}) {
+                    my $line = $stat->{name} . '/' . join(', ', @{$stat->{values}});
+                    push(@lines, $line);
+                }
+                print '[' . join(' | ', @lines)  . ']...';
+            }
+        }
+    }
+
+	return &sendData($key, $data);
+}
+
+sub processIntValue {
+    my ($key, $rJob) = @_;
+
+	my %data;
+	my $query = $rDatabases->{$rJob->{'database-id'}}->{dbh}->prepare($rJob->{query});
+	$query->execute() || die("Cannot execute query [" . $rJob->{query} . ']');
+	while (my $row = $query->fetchrow_hashref()) {
+		$data{value} = $row->{value};
+	}
+
+	return \%data;
+}
+
+sub sendData {
+	my ($key, $data) = @_;
+
+	if (!defined($data)) {
+	    # We don't send nulls!
+	    return 0;
+	}
+
+	my $uri = 'https://push.ducksboard.com/v/' . $key;
+
+	my $ua = LWP::UserAgent->new;
+	$ua->credentials("push.ducksboard.com:443","Ducksboard push API",$apiKey=>'x');
+
+	my $request = HTTP::Request->new( 'POST', $uri );
+	$request->header( 'Content-Type' => 'application/json' );
+	$request->content(encode_json($data));
+
+	my $response = $ua->request($request);
+	if ($response->is_success) {
+		return 1;
+	} else {
+		return 0;
+	}
 }
 
 sub isTimeForJob {
@@ -115,6 +217,7 @@ sub getDataFromConfig {
     my ($type, $rConfig) = @_;
     my $rOut;
     my $rConfigChunk = undef;
+
     if ($type eq 'database') {
         $rConfigChunk = $rConfig->{sources}->{database};
     } elsif ($type eq 'stat') {
@@ -127,13 +230,14 @@ sub getDataFromConfig {
             if ($type eq 'database') {
                 return getDatabaseFromConfig($rConfigChunk);
             } elsif ($type eq 'stat') {
-                return getDatabaseFromConfig($rConfigChunk);
+                return getStatFromConfig($rConfigChunk);
             }
         } else {
             foreach my $databaseConfigKey (keys(%{$rConfigChunk})) {
                 my $tmp = undef;
                 if ($type eq 'database') {
-                    $tmp = getStatFromConfig($rConfigChunk->{$databaseConfigKey});
+                    $rConfigChunk->{$databaseConfigKey}->{name} = $databaseConfigKey;
+                    $tmp = getDatabaseFromConfig($rConfigChunk->{$databaseConfigKey});
                 } elsif ($type eq 'stat') {
                     $tmp = getStatFromConfig($rConfigChunk->{$databaseConfigKey});
                 }
@@ -150,7 +254,7 @@ sub getDataFromConfig {
 sub getDatabaseFromConfig {
     my ($rConfig) = @_;
     my $rOut;
-    $rOut->{$rConfig->{id}} = $rConfig;
+   $rOut->{$rConfig->{id}} = $rConfig;
     return $rOut;
 }
 
